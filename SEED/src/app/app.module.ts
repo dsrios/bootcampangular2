55e49ca7c33c5a6c8f75a09938825b8916e59885
Diego Sebastian Rios Perez <diego.rios@yuxiglobal.com>
2018-11-27 19:11:50 -0500

    use github page cli

--- SEED/src/app/app.module.ts
+++ SEED/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
 import { SelectAnimalComponent } from './select-animal/select-animal.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardComponent } from './dashboard/card/card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MyComponentComponent,
-    SelectAnimalComponent
+    SelectAnimalComponent,
+    DashboardComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule

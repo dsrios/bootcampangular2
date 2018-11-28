8d8610987e280722cf3cdb9a4c6a9d8859f5776d
Diego Sebastian Rios Perez <diego.rios@yuxiglobal.com>
2018-11-28 16:08:00 -0500

    ngFor, input, output

--- SEED/src/app/dashboard/dashboard.component.ts
+++ SEED/src/app/dashboard/dashboard.component.ts
@@ -9,6 +9,14 @@ export class DashboardComponent implements OnInit {
 
   imageRender;
 
+  imageUrls = [
+    'https://media1.giphy.com/media/GWx7HFtKBAV3O/200w.webp?cid=3640f6095bfddeed6d6f577959d32263',
+    'https://media3.giphy.com/media/xAFIrLYSDMUa4/200w.webp?cid=3640f6095bfddeed6d6f577959d32263',
+    'https://media2.giphy.com/media/33zX3zllJBGY8/200.webp?cid=3640f6095bfddeed6d6f577959d32263',
+    'https://media2.giphy.com/media/KEh5kliRTSVJm/200w.webp?cid=3640f6095bfddeed6d6f577959d32263',
+    'https://media3.giphy.com/media/Ok4HaWlYrewuY/giphy.webp?cid=3640f6095bfddf014c3431362e77f639'
+  ];
+
   constructor() { }
 
   shownImageSelect( image) {
